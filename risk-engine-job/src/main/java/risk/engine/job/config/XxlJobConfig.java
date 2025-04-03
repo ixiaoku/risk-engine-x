a6096f48e9b446d7376b0d84d1de12f22600dfcc
x <dongchunrong@dongchunrongdeMacBook-Pro.local>
2025-04-03 10:19:43 +0800

    xxl-job config

--- risk-engine-job/src/main/java/risk/engine/job/config/XxlJobConfig.java
+++ risk-engine-job/src/main/java/risk/engine/job/config/XxlJobConfig.java
@@ -28,12 +28,6 @@ public class XxlJobConfig {
     @Value("${xxl.job.executor.address}")
     private String address;
 
-//    @Value("${xxl.job.executor.ip}")
-//    private String ip;
-//
-//    @Value("${xxl.job.executor.port}")
-//    private int port;
-
     @Value("${xxl.job.executor.logpath}")
     private String logPath;
 
@@ -48,8 +42,6 @@ public class XxlJobConfig {
         xxlJobSpringExecutor.setAccessToken(accessToken);
         xxlJobSpringExecutor.setAppname(appName);
         xxlJobSpringExecutor.setAddress(address);
-        //xxlJobSpringExecutor.setIp(ip);
-        //xxlJobSpringExecutor.setPort(port);
         xxlJobSpringExecutor.setLogPath(logPath);
         xxlJobSpringExecutor.setLogRetentionDays(logRetentionDays);
         return xxlJobSpringExecutor;
