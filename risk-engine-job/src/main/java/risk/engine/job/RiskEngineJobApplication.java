43cca1750bfcd47057dc80942840434aa738ea89
x <dongchunrong@dongchunrongdeMacBook-Pro.local>
2025-04-09 18:06:30 +0800

    Metric get

--- risk-engine-job/src/main/java/risk/engine/job/RiskEngineJobApplication.java
+++ risk-engine-job/src/main/java/risk/engine/job/RiskEngineJobApplication.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @Slf4j
 @SpringBootApplication()
-@ComponentScan(basePackages = {"risk.engine.job", "risk.engine.service", "risk.engine.db", "risk.engine.common", "risk.engine.crawler", "risk.engine.components"})
+@ComponentScan(basePackages = {"risk.engine.job", "risk.engine.service", "risk.engine.db", "risk.engine.common", "risk.engine.crawler", "risk.engine.components", "risk.engine.metric"})
 @MapperScan("risk.engine.db.dao")
 public class RiskEngineJobApplication {
 
