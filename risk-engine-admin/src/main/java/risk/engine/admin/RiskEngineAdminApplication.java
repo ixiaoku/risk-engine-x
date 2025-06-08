7a5277abfc5e60176e1150c142ce457dd5a5ad2e
x <dongchunrong@dongchunrongdeMacBook-Pro.local>
2025-06-08 22:09:57 +0800

    admin serve

--- risk-engine-admin/src/main/java/risk/engine/admin/RiskEngineAdminApplication.java
+++ risk-engine-admin/src/main/java/risk/engine/admin/RiskEngineAdminApplication.java
@@ -8,19 +8,19 @@ import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @Author: X
- * @Date: 2025/3/9 18:58
+ * @Date: 2025/6/9 18:58
  * @Version: 1.0
  */
 @Slf4j
 @SpringBootApplication()
-@ComponentScan(basePackages = {"risk.engine.rest", "risk.engine.service", "risk.engine.db", "risk.engine.common", "risk.engine.components", "risk.engine.metric"})
+@ComponentScan(basePackages = {"risk.engine.admin", "risk.engine.service", "risk.engine.db", "risk.engine.common", "risk.engine.components"})
 @MapperScan("risk.engine.db.dao")
 public class RiskEngineAdminApplication {
 
     public static void main(String[] args) {
         try {
             SpringApplication.run(RiskEngineAdminApplication.class, args);
-            log.info("HELLO WORLD ----->>> 服务rest启动成功 -------->>> SUCCESS" );
+            log.info("HELLO WORLD ----->>> 服务admin启动成功 -------->>> SUCCESS" );
         } catch (Exception e) {
             log.error("服务启动报错信息：{}", e.getMessage(), e);
             throw e;
