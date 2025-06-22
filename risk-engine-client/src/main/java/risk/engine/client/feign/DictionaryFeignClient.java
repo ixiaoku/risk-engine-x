1eb06d757ed2aafd9c3c373f12623b30c58b9f72
x <dongchunrong@dongchunrongdeMacBook-Pro.local>
2025-06-22 23:18:55 +0800

    admin serve

--- risk-engine-client/src/main/java/risk/engine/client/feign/DictionaryFeignClient.java
+++ risk-engine-client/src/main/java/risk/engine/client/feign/DictionaryFeignClient.java
@@ -1,8 +1,9 @@
 package risk.engine.client.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import risk.engine.dto.param.DictionaryParam;
 
 import java.util.Map;
 
@@ -13,11 +14,11 @@ import java.util.Map;
  */
 @FeignClient(name = "${spring.application.name}", url = "${risk.rest.feignClient.url}", contextId = "DictionaryFeignClient", path = "/dict")
 public interface DictionaryFeignClient {
+    
+    @PostMapping("/options")
+    Map<String, Object> getDict(@RequestBody DictionaryParam dictionaryParam);
 
-    @GetMapping("/options")
-    Map<String, Object> getDict(@RequestParam("dictKeyList") String[] dictKeyList);
-
-    @GetMapping("/db")
-    Map<String, Object> getDictDb(@RequestParam("dictKeyList") String[] dictKeyList, String queryCode);
+    @PostMapping("/db")
+    Map<String, Object> getDictDb(@RequestBody DictionaryParam dictionaryParam);
 
 }
--- risk-engine-rest/src/main/java/risk/engine/rest/controller/DictionaryFeignController.java
+++ risk-engine-rest/src/main/java/risk/engine/rest/controller/DictionaryFeignController.java
@@ -2,13 +2,11 @@ package risk.engine.rest.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import risk.engine.client.feign.DictionaryFeignClient;
 import risk.engine.common.function.ValidatorHandler;
 import risk.engine.dto.enums.ErrorCodeEnum;
+import risk.engine.dto.param.DictionaryParam;
 import risk.engine.service.service.IDictionaryService;
 
 import javax.annotation.Resource;
@@ -30,29 +28,29 @@ public class DictionaryFeignController implements DictionaryFeignClient {
 
     /**
      * 字典 不带查询条件
-     * @param dictKeyList 字典key数组
+     * @param dictionaryParam 字典key数组
      * @return 结果
      */
-    @GetMapping("/options")
+    @PostMapping("/options")
     @Override
-    public Map<String, Object> getDict(@RequestParam("dictKeyList") String[] dictKeyList) {
+    public Map<String, Object> getDict(@RequestBody DictionaryParam dictionaryParam) {
         ValidatorHandler.verify(ErrorCodeEnum.PARAMETER_IS_NULL)
-                .validateException(ObjectUtils.isEmpty(dictKeyList));
-        return dictionaryService.getList(dictKeyList);
+                .validateException(ObjectUtils.isEmpty(dictionaryParam.getDictKeyList()));
+        return dictionaryService.getList(dictionaryParam.getDictKeyList());
     }
 
     /**
      * 字典查询
-     * @param dictKeyList 参数
+     * @param dictionaryParam 参数
      * @return 参数
      */
-    @GetMapping("/db")
+    @PostMapping("/db")
     @Override
-    public Map<String, Object> getDictDb(@RequestParam("dictKeyList") String[] dictKeyList, @RequestParam("queryCode") String queryCode) {
+    public Map<String, Object> getDictDb(@RequestBody DictionaryParam dictionaryParam) {
         ValidatorHandler.verify(ErrorCodeEnum.PARAMETER_IS_NULL)
-                .validateException(ObjectUtils.isEmpty(dictKeyList)
+                .validateException(ObjectUtils.isEmpty(dictionaryParam.getDictKeyList())
                 );
-        return dictionaryService.getDictDb(dictKeyList, queryCode);
+        return dictionaryService.getDictDb(dictionaryParam.getDictKeyList(), dictionaryParam.getQueryCode());
     }
 
 }
